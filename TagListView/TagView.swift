c11793766ae6cc53d845fb99925e0650be2f896b
Lim TyTy <limtytyrupp@gmail.com>
2022-04-20 16:45:59 +0700

    add padding

--- TagListView/TagView.swift
+++ TagListView/TagView.swift
@@ -197,7 +197,7 @@ open class TagView: UIButton {
   override open var intrinsicContentSize: CGSize {
     var size = titleLabel?.text?.size(withAttributes: [NSAttributedString.Key.font: textFont]) ?? CGSize.zero
     size.height = textFont.pointSize + paddingY * 2
-    size.width += paddingX * 2
+    size.width += paddingX * 2 + 20
     if size.width < size.height {
       size.width = size.height
     }
